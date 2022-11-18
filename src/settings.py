0b53442d8a526c387a3bd96dad0a4cde2a76a62f
Bdioui Pierre <bdiouipierre@gmail.com>
2022-11-18 21:56:25 +0300

    link corrections

--- src/settings.py
+++ src/settings.py
@@ -117,12 +117,10 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = "m2ci.donotanswer@gmail.com"
+EMAIL_HOST_PASSWORD = 'hhnlavkwbajofzkr'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'CodingWithMitch Team <noreply@codingwithmitch.com>'
-
 
 BASE_URL = "http://167.71.235.158"
 
